e4ededf5b90babe1ce9282f542565e22ae60923b
Daniel Bergmann <brgmnn@users.noreply.github.com>
2016-12-28 19:20:08 -0800

    Moved content script to a new webpack entrypoint called load.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
-var path = require('path'); 
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry: './src/main.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'main.bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader',
-				query: {
-					presets: ['es2015']
-				}
-			}
-		]
-	},
-	stats: {
-		colors: true
-	},
-	devtool: 'source-map'
+  entry: {
+    load: './src/load.js',
+    main: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
 };
 
