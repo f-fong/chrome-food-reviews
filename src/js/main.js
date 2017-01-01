62f3abe0e1c12413d9b2e8a3228883228713d4d0
Felicia Fong <feliciasfong@gmail.com>
2016-12-31 16:40:35 -0800

    Got access to API (#10)
    * Got access to API
    
    * Fixed changes
    
    * Fixed more changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 import $ from 'jquery';
 
 alert('hello world!!!!', $);
+
+$(() => { 
+  setTimeout(() => {
+    $('div[class^=item_]').each(function() {
+      $(this).find('div[class^=content_]`').append('<p>hello</p>');
+    });
+  }, 15000);
+});
+
+
