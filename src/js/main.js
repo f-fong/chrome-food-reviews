76d27441233bf41b767f2379b36496fbfea25c58
Felicia Fong <feliciasfong@gmail.com>
2016-12-31 16:36:09 -0800

    Fixed changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,12 @@ import $ from 'jquery';
 
 alert('hello world!!!!', $);
 
-// $(function() {
-// 	$('.item_36eJsF').each(() => console.log('it works', $(this)));
-// });
-
 $(() => { 
-	setTimeout(() => {
-		$('div[class^=item_]').each(function() {
-			$(this).find('div[class^=content_]`').append('<p>hello</p>');
-		});
-	}, 15000);
+  setTimeout(() => {
+    $('div[class^=item_]').each(function() {
+      $(this).find('div[class^=content_]`').append('<p>hello</p>');
+    });
+  }, 15000);
 });
 
-/* App ID
-PSc31XzADb0QmeG8wrqS0g
-
-App Secret
-stckEgTOJIPWD7qzBz6vsIKIexfwZqMN7YRf8OYsSTv2zz255FzJH0iraLoX4Uka 
-*/
 
