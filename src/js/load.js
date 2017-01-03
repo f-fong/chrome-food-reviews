bfd9defe49a13bb76988a1464cd8a78b8b848c1d
Daniel Bergmann <brgmnn@users.noreply.github.com>
2017-01-02 20:11:35 -0800

    Made some improvements to storage.js.

--- src/js/load.js
+++ src/js/load.js
@@ -1,8 +1,14 @@
 /* global chrome */
+import Store from './storage';
 
 const s = document.createElement('script');
 s.src = chrome.extension.getURL('build/main.js');
 s.onload = () => {};
+
+const store = new Store(() => {
+  console.log('the access_token: ' + store.access_token);
+  // The access token is loaded from memory now.
+});
 
 // (document.head || document.documentElement).appendChild(s);
 document.head.appendChild(s);
